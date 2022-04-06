5696a086431f53e1b35f2a1e31c41e5857566dd8
HimanshiGulati <himanshigulati07@gmail.com>
2022-04-06 16:34:30 -0400

    Scaffolding the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="catorgies-container">
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
